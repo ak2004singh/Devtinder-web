6eb5fe446c2ebdbbadd2331555a08df334a5aeb5
ak2004-star <adityasingh67162@gmail.com>
2025-04-18 19:56:18 +0530

    dded nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import NavBar from './NavBar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       
+
+      <NavBar/>
       <h1>Aditya </h1>
       
       
